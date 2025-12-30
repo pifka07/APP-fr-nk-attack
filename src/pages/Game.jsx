20ec597919002e9e4055913191edbb4ab3703d51
pifka <wladiz2002@yahoo.de>
2025-12-30 19:48:17 +0000

    File changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -50,7 +50,7 @@ export default function Game() {
 
                 // Default config
                 let config = {
-                    poopTankCapacity: 10,
+                    poopTankCapacity: 11,
                     cooldownReduction: 0,
                     agility: 1,
                     comboDuration: 2000
@@ -61,7 +61,7 @@ export default function Game() {
                     if (upgrade) {
                         const totalEffect = upgrade.effect_per_level * pu.level;
                         switch(upgrade.key) {
-                            case 'poop_tank': config.poopTankCapacity = 10 + (pu.level * 3); break;
+                            case 'poop_tank': config.poopTankCapacity = 11 + (pu.level * 3); break;
                             case 'poop_cooldown': config.cooldownReduction = pu.level * 0.1; break;
                             case 'wing_speed': config.agility += totalEffect; break;
                             case 'combo_booster': config.comboDuration += (totalEffect * 1000); break;

a94ba836d1dc7739bf7627a330086866401bce1d
pifka <wladiz2002@yahoo.de>
2026-01-04 15:06:10 +0000

    File changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -269,7 +269,7 @@ export default function Game() {
             onMouseLeave={handleInputEnd}
         >
             {/* Game Engine Canvas */}
-            <div className="absolute top-[10px] bottom-[30px] left-0 right-0 z-0">
+            <div className="absolute top-0 bottom-0 left-0 right-0 z-0">
                 <GameEngine 
                     ref={engineRef}
                     config={gameConfig}

606447d3f45c0ca100a0dfb6658e7d4842dd0143
pifka <wladiz2002@yahoo.de>
2026-01-04 12:02:47 +0000

    Schwarze Balken vergrößert

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -259,7 +259,7 @@ export default function Game() {
 
     return (
         <div 
-            className="relative w-full h-screen bg-slate-900 overflow-hidden select-none touch-none"
+            className="relative w-full h-screen bg-slate-900 overflow-hidden select-none touch-none pt-[20px] pb-[20px]"
             onMouseDown={handleInputStart}
             onTouchStart={handleInputStart}
             onMouseMove={handleInputMove}
@@ -268,10 +268,6 @@ export default function Game() {
             onTouchEnd={handleInputEnd}
             onMouseLeave={handleInputEnd}
         >
-            {/* Black bars */}
-            <div className="absolute top-0 left-0 right-0 h-[20px] bg-black z-[100] pointer-events-none" />
-            <div className="absolute bottom-0 left-0 right-0 h-[20px] bg-black z-[100] pointer-events-none" />
-            
             {/* Game Engine Canvas */}
             <div className="absolute inset-0 z-0">
                 <GameEngine 

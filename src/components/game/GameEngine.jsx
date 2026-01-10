16f34b355b74c5c72cab481272727b7fa51a291d
pifka <wladiz2002@yahoo.de>
2026-01-10 17:42:55 +0000

    File changes

--- src/components/game/GameEngine.jsx
+++ src/components/game/GameEngine.jsx
@@ -243,7 +243,7 @@ const GameEngine = forwardRef(({ onGameOver, onScoreUpdate, onHealthUpdate, onCo
         IMAGES.current.parisForeground2.src = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6961111599b5db08cf38f4b2/13b759a24_2-Vordergrund2.png";
         IMAGES.current.parisForeground3.src = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6961111599b5db08cf38f4b2/8a922164e_2-Vordergrund3.png";
         IMAGES.current.parisBackground = new Image();
-        IMAGES.current.parisBackground.src = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6961111599b5db08cf38f4b2/de560f22c_2-Hintergrund2.png";
+        IMAGES.current.parisBackground.src = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6961111599b5db08cf38f4b2/82f77e60e_2-Hintergrund1.png";
 
         let loadedCount = 0;
         const checkLoad = () => {

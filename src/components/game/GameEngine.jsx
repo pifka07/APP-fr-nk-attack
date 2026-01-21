e0b4e9f9e7a589eff968ed41daca1271a991919c
pifka <wladiz2002@yahoo.de>
2026-01-21 16:56:38 +0000

    File changes

--- src/components/game/GameEngine.jsx
+++ src/components/game/GameEngine.jsx
@@ -1318,7 +1318,7 @@ const GameEngine = forwardRef(({ onGameOver, onScoreUpdate, onHealthUpdate, onCo
         // Draw Rooftop scrolling street
         if (level === 'rooftop' && isImageValid(IMAGES.current.rooftopStreet)) {
             const street = IMAGES.current.rooftopStreet;
-            const streetHeight = 200;
+            const streetHeight = 220;
             const streetScale = streetHeight / street.height;
             const streetWidth = street.width * streetScale;
             const streetY = height - streetHeight;

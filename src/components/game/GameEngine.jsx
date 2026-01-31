7c1d513c94c6612c352c53b959049253979e0d1a
pifka <wladiz2002@yahoo.de>
2026-01-31 13:55:00 +0000

    File changes

--- src/components/game/GameEngine.jsx
+++ src/components/game/GameEngine.jsx
@@ -1695,7 +1695,7 @@ const GameEngine = forwardRef(({ onGameOver, onScoreUpdate, onHealthUpdate, onCo
 
             state.berlinBuildings.forEach(building => {
                 if (isImageValid(building.img)) {
-                    const buildingY = groundY - building.height - 40; // 40 pixels higher
+                    const buildingY = groundY - building.height - 70; // 70 pixels higher
                     ctx.drawImage(building.img, building.x, buildingY, building.width, building.height);
                 }
             });
